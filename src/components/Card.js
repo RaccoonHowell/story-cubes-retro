b299894bf00f6cd84616f713727acc699a58c948
Rachel Howell <rac777blue@hotmail.com>
2022-08-29 17:20:44 +0100

    add react expanding textarea to card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,6 @@
 import React from "react"
 import ImageRow from "./ImageRow"
+import Textarea from "react-expanding-textarea"
 
 const Card = ({header}) => {
     return (
@@ -8,7 +9,7 @@ const Card = ({header}) => {
 
             <ImageRow />
 
-            <textarea/>
+            <Textarea className="textarea" />
         </>
     )
 }
