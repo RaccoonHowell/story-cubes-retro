4163294592d2f5b36a077e6eec88e8c4e2cc6ec7
Rachel Howell <rac777blue@hotmail.com>
2022-09-01 11:03:13 +0100

    delete drop target file and comment out strict mode in index.js

--- src/components/DraggableDiv.js
+++ src/components/DraggableDiv.js
@@ -1,10 +1,10 @@
 import React from "react"
 import { useDrag } from "react-dnd"
 
-const DraggableDiv = ({id, emoji}) => {
+const DraggableDiv = ({emoji}) => {
     const [{isDragging}, drag] = useDrag(() => ({
         type: "div",
-        item: {id: id, emoji: emoji},
+        item: {emoji: emoji},
         collect: (monitor) => ({
             isDragging: !!monitor.isDragging(),
         })
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,11 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
+import { useDrop } from "react-dnd"
 
 const Image = ({row}) => {
-    const [imageContent, setImageContent] = [""]
-    
+    const [imageContent, setImageContent] = useState("")
+ 
+    const [{isOver}, drop] = useDrop(() => ({
+        accept: "div",
+        drop: (item) => console.log(item),
+        // drop: (item) => setImageContent(item.emoji),
+        collect: (monitor) => ({
+            isOver: !!monitor.isOver(),
+        })
+    }))
+
     return (
-            <div className={`imgHolder gridSquare ${row}`}>
-                
+            <div 
+                ref={drop} 
+                className={`imgHolder gridSquare ${row}`}
+                style={{border: isOver ? "green solid 5px" : "black solid 1px"}} 
+            >
+                {imageContent}    
             </div>
     )
 }
