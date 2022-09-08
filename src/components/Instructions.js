8c12ae30353c6c2abda8b66dcdb71efdcf22ae9a
Rachel Howell <rac777blue@hotmail.com>
2022-09-08 13:40:15 +0100

    update spacing and use rem in css

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -5,11 +5,11 @@ const Instructions = () => {
             <section className="instructions">
                 <h3>How to play:</h3>
 
-                <p>Press 'Generate Images' and pick 3 images that best represent the time just before the sprint started (drag these to the 'Before Sprint' section)</p>
+                <p>Press 'Generate images' and pick 3 images that best represent the time just before the sprint started (drag these to the 'Before sprint' section)</p>
 
-                <p>Press 'Generate Images' again, and then pick 3 images that best represent the time during the sprint (drag these to the 'During Sprint' section)</p>
+                <p>Press 'Generate images' again, and then pick 3 images that best represent the time during the sprint (drag these to the 'During sprint' section)</p>
             
-                <p>Press 'Generate Images' again, and then pick 3 images that best represent what the future may hold for us (drag these to the 'Future Sprints' section)</p>
+                <p>Press 'Generate images' again, and then pick 3 images that best represent what the future may hold for us (drag these to the 'Future sprints' section)</p>
             </section>
     )
 }
